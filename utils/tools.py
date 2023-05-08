707b5bb341567b7b8d6164744ebd31ceaa6e31dc
GawarWawar <gawarwawar@gmail.com>
2023-05-08 23:47:27 +0300

    added utils.tools
    wrote funcrion to find multiple rows by multiple criteria
    didnt use it, needs improvements in calling

---
+++ utils/tools.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+
+def needed_rows(
+    df_to_look_for_indexes: pd.DataFrame,
+    criteries: list
+) -> pd.DataFrame:
+    """ Finds multiple rows by given criterias
+
+    Args:
+        df_to_look_for_indexes (pd.DataFrame): DataFrame in which we want to find rows\n
+        criteries (list): List of Dictionaries with "column" and "criteria" combination:
+            criteries = [{"column":"name_of_a_column", "criteria": criteria_value}]
+            Note: programe will do criterias in order, so add them into criteries accordingly
+
+    Returns:
+        (list): Returns list of needed indexes
+    """
+    for criteria in range(len(criteries)):
+        print(criteria)
+        df_to_look_for_indexes = df_to_look_for_indexes[
+            (df_to_look_for_indexes.loc[:,criteries[criteria]["column"]] == \
+                criteries[criteria]["criteria"])
+        ].copy()
+    return df_to_look_for_indexes

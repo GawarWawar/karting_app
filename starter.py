86cdae509a205092b74733644f5611364b63ff84
GawarWawar <gawarwawar@gmail.com>
2023-05-08 22:22:22 +0300

    checked and implimented car_change
    changes car from 0 to a new car after start of the race or after the pit stop

--- starter.py
+++ starter.py
@@ -36,7 +36,7 @@ initial_total_race_time = body_content["onTablo"]["totalRaceTime"]
 race_started_button_timestamp = body_content["onTablo"]['raceStartedButtonTimestamp']
 race_finished_timestamp = body_content["onTablo"]['raceFinishedTimestamp']
 
-only_one_cycle = False
+only_one_cycle = 1
 
 teams_stats = body_content["onTablo"]["teams2"]
 
@@ -45,7 +45,9 @@ for team in teams_stats:
     last_lap_info_update = {
         team: {
             "last_lap": 0,
-            "last_kart": 0
+            "last_kart": 0,
+            "current_segment": 1,
+            "was_on_pit" : False
         }
     }
     last_lap_info.update(last_lap_info_update)
@@ -56,28 +58,33 @@ last_lap_info = None
 
 while (
     race_started_button_timestamp>race_finished_timestamp
-) and not(only_one_cycle):
-    if initial_total_race_time != body_content["onTablo"]["totalRaceTime"] or not(only_one_cycle):
+) and not (only_one_cycle == 0):
+    if initial_total_race_time != body_content["onTablo"]["totalRaceTime"] or not (only_one_cycle == 0):
         for team in teams_stats:
-            lap_count = teams_stats[str(team)]["lapCount"]
+            lap_count = teams_stats[team]["lapCount"]
             if lap_count !=0:
-                pilot_name = teams_stats[str(team)]["pilotName"]
-                kart = teams_stats[str(team)]["kart"]
-                last_lap_time = teams_stats[str(team)]["lastLap"]
-                last_lap_s1_time = teams_stats[str(team)]["lastLapS1"]
-                last_lap_s2_time = teams_stats[str(team)]["lastLapS2"]
+                pilot_name = teams_stats[team]["pilotName"]
+                kart = teams_stats[team]["kart"]
+                last_lap_time = teams_stats[team]["lastLap"]
+                last_lap_s1_time = teams_stats[team]["lastLapS1"]
+                last_lap_s2_time = teams_stats[team]["lastLapS2"]
 
-                if int(kart) == 0:
+                if int(kart) == 0 or int(kart) != df_last_lap_info.loc[team, "last_kart"]:
                     true_kart = False
                 else: 
                     true_kart = True
-                    # NEED TO ADD SOME FUNCTIONALITY TO CHANGE TRUE_CART INTO TRUE
-                        # IN THE TABLE ITSELF, TO CHANGE TO THE INITIAL CART NUMBER
-                
-                # NEED TO ADD FUNCTIONALITY FOR is_on_pit = True SCENARIO
-                is_on_pit = teams_stats[str(team)]["isOnPit"]
-                if is_on_pit:
-                    pass
+                    needed_indexes = df_statistic[
+                        (df_statistic.loc[:,"pilot_name"] == pilot_name) 
+                        &(df_statistic.loc[:,"true_kart"] == False) 
+                        &(
+                            (df_statistic.loc[:,"kart"] == 0) |
+                            (df_statistic.loc[:,"kart"] ==\
+                                df_last_lap_info.loc[str(team), "last_kart"])
+                        )
+                    ].index
+                    df_statistic.loc[needed_indexes, "true_kart"] = True
+                    df_statistic.loc[needed_indexes, "kart"] = int(kart)
+                    df_last_lap_info.loc[team, "last_kart"] = kart
                 
                 if int(lap_count) > int(
                     df_last_lap_info.loc[team].at["last_lap"]
@@ -86,18 +93,29 @@ while (
                         df_statistic,
                         [
                             pilot_name, # pilot_name
-                            kart, # kart
+                            int(kart), # kart
                             lap_count, 
                             last_lap_time, # lap_time
                             last_lap_s1_time, # s1
                             last_lap_s2_time, # s2
-                            true_kart # Flag to check if kart is true or still 0
+                            df_last_lap_info.loc[team, "current_segment"], #team_segment
+                            true_kart, # Flag to check if kart is true or still 0
                         ]
                     )
-                    df_last_lap_info.loc[team].at["last_lap"] = lap_count
+                    
+                    # NEED TO ADD FUNCTIONALITY FOR is_on_pit = True SCENARIO
+                    is_on_pit = teams_stats[team]["isOnPit"]
+                    if is_on_pit:
+                        df_last_lap_info.loc[team, "current_segment"] += 1
+                        df_last_lap_info.loc[team, "was_on_pit"] = True
+                        
+                        
+                    
+                    
+                    df_last_lap_info.loc[team, "last_lap"] = lap_count
                 
-                best_lap_on_segment = teams_stats[str(team)]["bestLapOnSegment"]
-                mid_lap = teams_stats[str(team)]["midLap"]
+                best_lap_on_segment = teams_stats[team]["bestLapOnSegment"]
+                mid_lap = teams_stats[team]["midLap"]
         
     # RENEW VARIABLES FOR THE NEXT CYCLE
     race_started_button_timestamp = body_content["onTablo"]['raceStartedButtonTimestamp']
@@ -105,7 +123,12 @@ while (
     
     initial_total_race_time == body_content["onTablo"]["totalRaceTime"]
     
-    only_one_cycle = True
+    only_one_cycle -= 1
     # MAKE NEW REQUEST
 
 print(df_statistic)
+
+
+
+print(df_statistic)
+

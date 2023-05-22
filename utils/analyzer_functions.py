d2f106eb3bb2303438b6611eb39c95ca2b35288d
GawarWawar <gawarwawar@gmail.com>
2023-05-22 19:55:36 +0300

    create a separate module for regression_process

--- utils/analyzer_functions.py
+++ utils/analyzer_functions.py
@@ -14,7 +14,6 @@ import importlib.util
 if __name__ == "__main__":
     import add_row
     import tools as u_tools
-    import regression_evaluation as regres_eval
 else:
     spec = importlib.util.spec_from_file_location("add_row", "utils/add_row.py")
     add_row = importlib.util.module_from_spec(spec)
@@ -25,11 +24,6 @@ else:
     u_tools = importlib.util.module_from_spec(spec)
     sys.modules["u_tools"] = u_tools
     spec.loader.exec_module(u_tools)
-    
-    spec = importlib.util.spec_from_file_location("regres_eval", "utils/regression_evaluation.py")
-    regres_eval = importlib.util.module_from_spec(spec)
-    sys.modules["regres_eval"] = regres_eval
-    spec.loader.exec_module(regres_eval)
 
 def records_columns_to_numeric (
     df_of_records: pd.DataFrame,
@@ -44,6 +38,7 @@ def records_columns_to_numeric (
             df_of_records[column]=pd.to_numeric(df_of_records[column])
     
     return df_of_records
+
 
 def module_to_create_df_with_statistic(
     df_of_records: pd.DataFrame, 
@@ -76,8 +71,7 @@ def module_to_create_df_with_statistic(
             ] = all_features_records.loc[:, column_name_to_look_for_values_in].min()
         
     return df_with_features
-        
-        
+
 
 def module_to_create_pilot_statistics (
     df_of_records: pd.DataFrame,
@@ -179,67 +173,3 @@ def module_to_create_karts_statistics_for_every_pilot(
             [df_pilot_on_karts, karts_of_pilot_df]   
         )
     return df_pilot_on_karts
- 
-def regression_process(
-      df_to_analyze: pd.DataFrame,  
-):
-    x = df_to_analyze.iloc[:, :-1].values # Matrix of Features
-    y = df_to_analyze.iloc[:, -1].values # Depending variable vector
-    
-    # Encoding the Independent Variable
-    from sklearn.compose import ColumnTransformer
-    from sklearn.preprocessing  import OneHotEncoder
-    
-    ohe = OneHotEncoder()
-    ct = ColumnTransformer(
-        transformers=[("encoder", ohe, [0,1])],
-        remainder="passthrough"
-    )
-    x = ct.fit_transform(x).toarray()
-    
-    # Splitting the dataset into the Training set and Test set
-    from sklearn.model_selection import train_test_split
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
-    
-    print_prediction = False
-    
-    regres_eval.multiple_linear_regression(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        print_prediction
-    )
-    
-    regres_eval.polinomial_regression(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        print_prediction
-    )
-    
-    regres_eval.support_vector_regression(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        print_prediction
-    )
-    
-    regres_eval.decision_tree_regression(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        print_prediction
-    )
-    
-    regres_eval.random_forest_regression(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        print_prediction
-    )
-    

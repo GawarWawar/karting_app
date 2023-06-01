b53ef7dca328bcbbdbc1c0c4cdbc5113d674a9ca
GawarWawar <gawarwawar@gmail.com>
2023-06-01 19:32:36 +0300

    coeficient is created
    coeficient of every pilot who took place in at least 1 VP is created

--- pilot_rating.py
+++ pilot_rating.py
@@ -1,16 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from dateutil import parser
 
 import time
 
-import sys
 import os
 
-from sklearn.preprocessing import Normalizer
-
-individual_pilot_names_df = pd.DataFrame(
+individual_pilot_statistic_df = pd.DataFrame(
     {
         "pilot": pd.Series(dtype=str)
     }
@@ -36,14 +31,14 @@ for race_numbe_file_tuple in enumerate(files_to_read):
             }
         )
         
-        individual_pilot_names_df = pd.concat(
+        individual_pilot_statistic_df = pd.concat(
             [
-                individual_pilot_names_df,
+                individual_pilot_statistic_df,
                 df_pilots_to_concate
             ]
         )
         
-        individual_pilot_names_df = individual_pilot_names_df.drop_duplicates(
+        individual_pilot_statistic_df = individual_pilot_statistic_df.drop_duplicates(
             "pilot",ignore_index=True, keep="first"
         )
         
@@ -53,46 +48,31 @@ for race_numbe_file_tuple in enumerate(files_to_read):
                     "lap_time"
                 ]
             lap_time = lap_time.values
-            individual_pilot_names_df.loc[
-                individual_pilot_names_df.loc[:, "pilot"] == pilot,
+            individual_pilot_statistic_df.loc[
+                individual_pilot_statistic_df.loc[:, "pilot"] == pilot,
                 f"race_{race_numbe_file_tuple[0]+1}"
             ] = lap_time
         
     except pd.errors.EmptyDataError:
         pass
- 
-        
-individual_pilot_names_df.to_csv("test.csv")
 
-pilots_records_in_VP_df = pd.read_csv(
-    "pilot_rating.csv",
-    dtype={
-        "pilot": str,
-        "lap_time": float
-    }
-)
+for race_as_column in individual_pilot_statistic_df:
+    if race_as_column != "pilot":
+        max_temp = individual_pilot_statistic_df.loc[:, race_as_column].max()
+        min_temp = individual_pilot_statistic_df.loc[:, race_as_column].min()
+        for temp in range(len(individual_pilot_statistic_df.loc[:, race_as_column])):
+            normilezed_temp =(
+                (individual_pilot_statistic_df.loc[temp, race_as_column]-min_temp)
+                /
+                (max_temp-min_temp)
+            )
+            normilezed_temp = float(f"{normilezed_temp:.4f}")
+            individual_pilot_statistic_df.loc[temp, race_as_column] = normilezed_temp
 
-pilots_mean_time_across_all_VP_df = pilots_records_in_VP_df.loc[:, "pilot"].drop_duplicates().copy().T.to_frame().reset_index(drop=True)
-for pilot in pilots_mean_time_across_all_VP_df.loc[:, "pilot"]:
-    pilots_mean_time_across_all_VP_df.loc[
-        pilots_mean_time_across_all_VP_df.loc[:, "pilot"] == pilot,
-        "temp"    
-    ] = pilots_records_in_VP_df.loc[
-            pilots_records_in_VP_df.loc[:, "pilot"] == pilot, 
-            "lap_time"
-        ].mean()
+for index in individual_pilot_statistic_df.loc[:, "pilot"].index:
+    average_pilot_coeficient = individual_pilot_statistic_df.iloc[index, 1:-1].mean()
+    average_pilot_coeficient = float(f"{average_pilot_coeficient:.4f}")
+    individual_pilot_statistic_df.loc[index, "coeficient"] = average_pilot_coeficient
 
-pilots_mean_time_across_all_VP_df["normilized_temp"] = pilots_mean_time_across_all_VP_df["temp"].copy()
-max_temp = pilots_mean_time_across_all_VP_df.loc[:, "normilized_temp"].max()
-min_temp = pilots_mean_time_across_all_VP_df.loc[:, "normilized_temp"].min()
-
-for temp in range(0, len(pilots_mean_time_across_all_VP_df.loc[:, "normilized_temp"])):
-    normilezed_temp = (pilots_mean_time_across_all_VP_df.loc[temp, "normilized_temp"]-min_temp)/(max_temp-min_temp)
-    pilots_mean_time_across_all_VP_df.loc[temp, "normilized_temp"] = normilezed_temp
-    
-pilots_mean_time_across_all_VP_df.sort_values(by="normilized_temp",inplace=True,ignore_index=True)
-
-#pilots_mean_time_across_all_VP_df = pilots_mean_time_across_all_VP_df.values
-#norm = Normalizer(norm="max")
-#pilots_mean_time_across_all_VP_df[:, 1] = norm.fit_transform([pilots_mean_time_across_all_VP_df[:, 1]])
+individual_pilot_statistic_df.to_csv("test.csv", index=False, index_label=False)
 

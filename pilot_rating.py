be74578f72a86d2514bc0f23267ae3bef567d62e
GawarWawar <gawarwawar@gmail.com>
2023-06-01 18:06:33 +0300

    optimized the process of creation df with pilot statistic
    now we have only one cycle, that:
    -reading race statistic from file
    -adding all pilot from that race
    -droping duplicates of pilots
    - adding lap time for this pilots into the df

--- pilot_rating.py
+++ pilot_rating.py
@@ -10,64 +10,57 @@ import os
 
 from sklearn.preprocessing import Normalizer
 
-path_to_records = "data/temp_by_races"
-files_to_read = os.listdir(path_to_records)
-dict_of_df_of_races = {}
-for file in enumerate(files_to_read):
-    try:
-        df_to_add = pd.read_csv(
-            path_to_records+"/"+file[1],
-            dtype={
-                "team": int,
-                "pilot": str,
-                "kart": int,
-                "lap": int,
-                "lap_time": float,
-                "s1": float,
-                "s2": float,
-                "segment": int,
-                "true_name": bool,
-                "true_kart": bool,
-            }
-        )
-        
-        dict_with_df_to_add = {
-            file[0]: df_to_add
-        }
-        
-        dict_of_df_of_races.update(dict_with_df_to_add)
-    except pd.errors.EmptyDataError:
-        pass
-
 individual_pilot_names_df = pd.DataFrame(
     {
         "pilot": pd.Series(dtype=str)
     }
 )
-for df in dict_of_df_of_races:
-    df_to_concate = dict_of_df_of_races[df].loc[:, "pilot"].T.to_frame().copy()
-    individual_pilot_names_df = pd.concat(
-        [
-            individual_pilot_names_df,
-            df_to_concate
-        ]
-    )
 
-individual_pilot_names_df = individual_pilot_names_df.drop_duplicates(ignore_index=True)
-
-for df in range(len(dict_of_df_of_races)):
-    individual_pilot_names_df[f"race_{df+1}"] = np.nan
-    for pilot in dict_of_df_of_races[df].loc[:, "pilot"]:
-        lap_time = dict_of_df_of_races[df].loc[
-                dict_of_df_of_races[df].loc[:, "pilot"] == pilot,
-                "lap_time"
+path_to_records = "data/temp_by_races"
+files_to_read = os.listdir(path_to_records)
+files_to_read.reverse()
+dict_of_df_of_races = {}
+for race_numbe_file_tuple in enumerate(files_to_read):
+    try:
+        race_statistic_df = pd.read_csv(
+            path_to_records+"/"+race_numbe_file_tuple[1],
+            dtype={
+                "pilot": str,
+                "lap_time": float,
+            }
+        )
+        
+        df_pilots_to_concate = pd.DataFrame(
+            {
+                "pilot": race_statistic_df["pilot"].copy()
+            }
+        )
+        
+        individual_pilot_names_df = pd.concat(
+            [
+                individual_pilot_names_df,
+                df_pilots_to_concate
             ]
-        if not lap_time.empty:
+        )
+        
+        individual_pilot_names_df = individual_pilot_names_df.drop_duplicates(
+            "pilot",ignore_index=True, keep="first"
+        )
+        
+        for pilot in race_statistic_df.loc[:, "pilot"]:
+            lap_time = race_statistic_df.loc[
+                    race_statistic_df.loc[:, "pilot"] == pilot,
+                    "lap_time"
+                ]
             lap_time = lap_time.values
             individual_pilot_names_df.loc[
                 individual_pilot_names_df.loc[:, "pilot"] == pilot,
-                f"race_{df}"
+                f"race_{race_numbe_file_tuple[0]+1}"
             ] = lap_time
+        
+    except pd.errors.EmptyDataError:
+        pass
+ 
         
 individual_pilot_names_df.to_csv("test.csv")
 

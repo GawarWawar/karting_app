0f1fbf7e92456fdf1e2bf0f8c6b257cfce691f06
GawarWawar <gawarwawar@gmail.com>
2023-06-26 12:37:28 +0300

    added signals to tasks
    - started experimenting with signals
    - give task custom name to easier emplement it

--- karting_site/recorder/tasks.py
+++ karting_site/recorder/tasks.py
@@ -1,26 +1,35 @@
 from celery import current_app, Celery, shared_task
-from celery.signals import after_task_publish
+from celery.signals import after_task_publish, task_postrun, task_success
+
+from celery.result import AsyncResult
 
 import time
 
-@after_task_publish.connect
-def update_sent_state(sender=None, headers=None, **kwargs):
-    # the task may not exist if sent using `send_task` which
-    # sends tasks by name, so fall back to the default result backend
-    # if that is the case.
-    task = current_app.tasks.get(sender)
-    backend = task.backend if task else current_app.backend
- 
-    backend.store_result(headers['id'], None, "SENT")
+from . import models
+
+
 
 @shared_task
 def wait(delay):
     time.sleep(delay)
     return "Done"
 
-@shared_task(task_track_started=True)
-def hello():
+@shared_task(name = "recorder.hello", bind = True)
+def hello(self):
     time.sleep(15)
     hello = "Hello!"
     return hello
 
+@after_task_publish.connect(sender=hello.name)
+def task_sent_handler(sender=None, headers=None, body=None, **kwargs):
+    # information about task are located in headers for task messages
+    # using the task protocol version 2.
+    print("Hi!")
+    info = headers if 'task' in headers else body
+    
+@task_success.connect(sender=hello.name)
+def task_success_handler(sender=None, headers=None, body=None, **kwargs):
+    # information about task are located in headers for task messages
+    # using the task protocol version 2.
+    print("Good Bye!")
+    info = headers if 'task' in headers else body

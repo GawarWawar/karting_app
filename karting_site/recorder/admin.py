91a67b76d3976c21df4309798987ced88ffe70f1
GawarWawar <gawarwawar@gmail.com>
2023-06-28 01:29:54 +0300

    added racerecords into admin
    -added there filters:
    -- by race
    -- by certain name in the race

--- karting_site/recorder/admin.py
+++ karting_site/recorder/admin.py
@@ -1,18 +1,68 @@
+from collections.abc import Iterator
+from typing import Any
 from django.contrib import admin
 from django.http import HttpResponse, HttpResponseRedirect
+from django.utils.datastructures import MultiValueDictKeyError
 
 from django_celery_results.models import TaskResult
-
 from . import models
 from . import tasks
 from .utils import tools as u_tools
 from . import recorder
+    
+class NamesInRaceFilter(admin.SimpleListFilter):
+    title = 'Names in the race'
+    parameter_name = 'name_of_a_pilot'
 
-# Register your models here.
+    def lookups(self, request, model_admin):
+        try:
+            query = model_admin.get_queryset(request).filter(race = request.GET.__getitem__("race__id__exact"))
+        except MultiValueDictKeyError: 
+            return None
+        else:
+            query = query.values("pilot_name").distinct()
+            list_to_return = []
+            for value in query:
+                list_to_return.append([f"{value['pilot_name']}",value['pilot_name']])
+        
+            return tuple(list_to_return)
+
+    def queryset(self, request, queryset):
+        try:
+            queryset = queryset.filter(pilot_name = request.GET.__getitem__("name_of_a_pilot"))
+        except MultiValueDictKeyError: 
+            pass
+        return queryset    
+
+# Register your models here.    
+class RaceRecordsAdmin(admin.ModelAdmin):
+    list_display = [
+        "race",
+        "team_number",
+        "name_of_a_pilot",
+        "kart",
+        "lap_count",
+        "lap_time",
+        "s1_time",
+        "s2_time",
+        "team_segment",
+        "true_name",
+        "true_kart",
+    ]
+    
+    list_filter = ( 
+        "race", 
+        NamesInRaceFilter
+    )
+    
+    def name_of_a_pilot(self, obj):
+        return obj.pilot_name
+        
 class RaceAdmin(admin.ModelAdmin):
     fields = [
         "name_of_the_race",
     ]
+    
     list_display = [
         "name_of_the_race", 
         "publish_date", 
@@ -41,3 +91,5 @@ class RaceAdmin(admin.ModelAdmin):
         return super().response_change(request, obj)
 
 admin.site.register(models.Race, RaceAdmin)
+admin.site.register(models.RaceRecords, RaceRecordsAdmin)
+

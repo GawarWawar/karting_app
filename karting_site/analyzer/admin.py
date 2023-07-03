a2ab913c85ead4dfd607b17f51c4a22395864c84
GawarWawar <gawarwawar@gmail.com>
2023-07-03 23:36:55 +0300

    created analyzer models and added them to admin
    - created models for:
    -- races, in which will be info about the race
    -- pilots, in which will be stats of pilots, that took place in the race
    -- race types, in which will be types of the race, to select when races will be created

--- karting_site/analyzer/admin.py
+++ karting_site/analyzer/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from . import models
 # Register your models here.
+class VelikiPeregoniAdmin(admin.ModelAdmin):
+    list_display= [
+        "name_of_the_race",
+        "race_class",
+        "date_of_race",
+    ]
+    
+    
+class PilotsInVPAdmin (admin.ModelAdmin):
+    list_display = [
+       
+    ]
+    
+class TypeOfVPAdmin (admin.ModelAdmin):
+    list_display = [
+        "name_of_the_race_class"
+    ]
+    
+admin.site.register(models.VelikiPeregoni, VelikiPeregoniAdmin)
+admin.site.register(models.PilotsInVP, PilotsInVPAdmin)
+admin.site.register(models.TypesOfVP, TypeOfVPAdmin)
--- karting_site/analyzer/models.py
+++ karting_site/analyzer/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+import datetime
 # Create your models here.
+class TypesOfVP (models.Model):
+    name_of_the_race_class = models.CharField(("Class of the race"), max_length=50, default=None, unique=True)
+    
+    def __str__(self):
+        return self.name_of_the_race_class
+
+class VelikiPeregoni (models.Model):
+    name_of_the_race = models.CharField(("Race name"), max_length=50, default=None, null=True)
+    race_class = models.ForeignKey(TypesOfVP, to_field="name_of_the_race_class", on_delete=models.CASCADE)
+    date_of_race = models.DateField(("Date of race"), auto_now=False, auto_now_add=False, default=datetime.datetime.now())
+    
+    def __str__(self):
+        return self.name_of_the_race    
+    
+class PilotsInVP (models.Model):  
+    race = models.ForeignKey(VelikiPeregoni, on_delete=models.CASCADE)
+    pilot = models.CharField(("Pilot"), max_length=100)
+    avarage_lap_time = models.FloatField(("Avarage Lap Time"))

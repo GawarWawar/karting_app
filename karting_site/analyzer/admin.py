0eb8df1e73af786928783d28157c84a281720be2
GawarWawar <gawarwawar@gmail.com>
2023-07-04 22:04:12 +0300

    transferred csv import from TempOfPilotsInVPAdmin to VelikiPeregoniAdmin
    - now when csv is imported, race could be created or chosen by name, not id
    - also race_class should be specified upon the creation

--- karting_site/analyzer/admin.py
+++ karting_site/analyzer/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import redirect, render
+from django.db.utils import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 
 from django import forms
 
@@ -11,6 +13,11 @@ from . import models
 class CsvImportForm(forms.Form):
     csv_file = forms.FileField()
 
+
+class TempOfPilotsInVPInline(admin.TabularInline):
+    model = models.TempOfPilotsInVP
+    extra = 0
+
 # Register your models here.
 class VelikiPeregoniAdmin(admin.ModelAdmin):
     list_display= [
@@ -20,14 +27,11 @@ class VelikiPeregoniAdmin(admin.ModelAdmin):
         "date_of_race",
     ]
     
-class TempOfPilotsInVPAdmin (admin.ModelAdmin):
-    change_list_template = "pilots_changelist.html"
-    
-    list_display = [
-        "race",
-        "pilot",
-        "average_lap_time",
+    inlines = [
+        TempOfPilotsInVPInline
     ]
+    
+    change_list_template = "veliki_peregoni_changelist.html"    
     
     def get_urls(self):
         urls = super().get_urls()
@@ -35,17 +39,26 @@ class TempOfPilotsInVPAdmin (admin.ModelAdmin):
             path('import-csv/', self.import_csv),
         ]
         return my_urls + urls
-
+    
     def import_csv(self, request):
         if request.method == "POST":
-            race_id = request.POST["race_id"]
+            print(request.POST)
+            race_name = request.POST["race_name"]
             csv_file = request.FILES["csv_file"]
             csv_content = pd.read_csv(csv_file)
+            try:
+                race = models.VelikiPeregoni.objects.get(name_of_the_race = race_name)
+            except (IntegrityError, ObjectDoesNotExist):
+                race = models.VelikiPeregoni(name_of_the_race = race_name)
+            
+            if request.POST["race_class_id"]:
+                print(request.POST["race_class_id"])
+                race_class = models.TypesOfVP.objects.get(pk = request.POST["race_class_id"])
+                race.race_class = race_class
             for pilot in list(csv_content.loc[:, "pilot"].index):
-                race = models.VelikiPeregoni.objects.get(pk=race_id)
                 pilot_name = csv_content.at[pilot, "pilot"]
                 average_lap_time = csv_content.at[pilot, "average_lap_time"]
-                pilot = models.PilotsInVP(
+                pilot = models.TempOfPilotsInVP(
                     race = race,
                     pilot = pilot_name,
                     average_lap_time = average_lap_time
@@ -54,10 +67,22 @@ class TempOfPilotsInVPAdmin (admin.ModelAdmin):
             self.message_user(request, "Your csv file has been imported")
             return redirect("..")
         form = CsvImportForm()
-        payload = {"form": form}
+        types_of_races = models.TypesOfVP.objects.all().values()
+        payload = {
+            "form": form,
+            "types_of_races": types_of_races,
+            "abc": 123
+        }
         return render(
-            request, "csv_form.html", payload
+            request, "csv_form_veliki_peregoni.html", payload
         )
+    
+class TempOfPilotsInVPAdmin (admin.ModelAdmin):
+    list_display = [
+        "race",
+        "pilot",
+        "average_lap_time",
+    ]
     
 class TypeOfVPAdmin (admin.ModelAdmin):
     list_display = [

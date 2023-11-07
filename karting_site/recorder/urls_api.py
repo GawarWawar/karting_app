d46c531aef63f118e961f63113b6ad6e55054da8
GawarWawar <gawarwawar@gmail.com>
2023-11-07 20:09:49 +0200

    made api/recorder/last_race in the same form as other race_records calls
    - was: api/recorder/last_race ->
    - now: api/recorder/race_records/last_race

--- karting_site/recorder/urls_api.py
+++ karting_site/recorder/urls_api.py
@@ -4,7 +4,7 @@ from . import views_api
 
 app_name = "recorder"
 urlpatterns = [
-    path("last_race", views_api.last_race_records_api, name="last_race_records_api"),
     path("list_of_all_races", views_api.list_of_all_races_page_api, name="list_of_all_races_page_api"),
+    path("race_records/last_race", views_api.last_race_records_api, name="last_race_records_api"),
     path("race_records/<race_id>", views_api.view_race_records_by_id_api, name="view_race_records_by_id_api")
 ]

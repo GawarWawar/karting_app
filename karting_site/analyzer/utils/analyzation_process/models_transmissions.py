21deb424222c21c38cafb3a2cecca7a49ca5c036
GawarWawar <gawarwawar@gmail.com>
2023-11-26 22:56:41 +0200

    added collect_BR_temp_records_into_DataFrame
    - this is temporary function
    - it collects records of BigRaces for the coeficient creation

--- karting_site/analyzer/utils/analyzation_process/models_transmissions.py
+++ karting_site/analyzer/utils/analyzation_process/models_transmissions.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 
 from recorder import models as recorder_models
-
+from analyzer import models as analyzer_models
 
 def collect_race_records_into_DataFrame (
     race_id: int,
@@ -38,3 +38,25 @@ def collect_race_records_into_DataFrame (
         df_from_recorded_records.pop("race")
     
     return df_from_recorded_records
+
+def collect_BR_temp_records_into_DataFrame (
+    race_id: int,
+    purge_models_technical_columns:bool = True
+):
+    big_race = analyzer_models.TempOfPilotsInBR.objects.filter(race = race_id).values_list()
+    df_of_race_temps = pd.DataFrame.from_records(
+        big_race, 
+        columns=[
+            "id",
+            "race",
+            "pilot",
+            "average_lap_time"
+        ]
+    )
+    
+    if purge_models_technical_columns:
+        # Delete columns from model
+        df_of_race_temps.pop("id")
+        df_of_race_temps.pop("race")
+    
+    return df_of_race_temps

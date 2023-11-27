760c8f0975c0d463a8c4cbc005a4ba41f241813c
GawarWawar <gawarwawar@gmail.com>
2023-11-27 20:57:36 +0200

    reworked add_coeficients_and_temp_from_average_coeficient_to_df
    - reworked function:
    -- was: combining dfs with for cycle and inefficient functions
    -- now: the vector methods merge, fillna, mean and apply
    - deleted unneeded functions from file:
    - get_pilot_coeficient_from_df_of_primary_coeficient
    - create_avarage_coeficient

--- karting_site/analyzer/utils/analyzation_process/coeficient_creation_functions.py
+++ karting_site/analyzer/utils/analyzation_process/coeficient_creation_functions.py
@@ -87,37 +87,6 @@ def create_primary_coeficient ():
     print(en_t-st_t)
     return individual_pilot_statistic_df
 
-def get_pilot_coeficient_from_df_of_primary_coeficient(
-    df_to_create_coeficients_into: pd.DataFrame,
-    df_of_primary_coeficient: pd.DataFrame,
-    
-    pilot_index_in_df_to_create_coeficients_into:list,
-    pilot: str
-) -> list:
-    this_race_coeficient = df_of_primary_coeficient.loc[
-                df_of_primary_coeficient.loc[:, "pilot"] == pilot,
-                "coeficient"
-            ]
-    if not this_race_coeficient.empty:
-        return this_race_coeficient.values
-    else:
-        this_race_coeficient = df_to_create_coeficients_into.loc[
-            pilot_index_in_df_to_create_coeficients_into,
-            "this_race_coeficient"
-        ]
-        return this_race_coeficient.values
-
-def create_avarage_coeficient(
-    this_race_coeficient: float,
-    pilot_coeficient: float,
-) -> float:
-    average_coeficient = (
-            this_race_coeficient
-        +
-            pilot_coeficient
-        )/2 
-    return average_coeficient
-
 def make_temp_from_average_coeficient(
     average_coeficient: float,
     max_temp: float,
@@ -149,48 +118,28 @@ def add_coeficients_and_temp_from_average_coeficient_to_df (
                 min_temp = min_temp
             )
     
-    max_temp = df_to_create_coeficients_into["pilot_temp"].max()
-    min_temp = df_to_create_coeficients_into["pilot_temp"].min()
+    df_to_create_coeficients_into = pd.merge(
+        df_to_create_coeficients_into,
+        df_of_primary_coeficient,
+        on="pilot",
+        how="left",
+    )
     
-    for pilot in df_to_create_coeficients_into.loc[:, "pilot"]:
-        pilot_index_in_df_to_create_coeficients_into = df_to_create_coeficients_into.loc[
-                df_to_create_coeficients_into.loc[:, "pilot"] == pilot,
-                "pilot"
-            ].index
-        df_to_create_coeficients_into.loc[
-                pilot_index_in_df_to_create_coeficients_into,
-                "pilot_coeficient"
-        ] = get_pilot_coeficient_from_df_of_primary_coeficient(
-            df_to_create_coeficients_into=df_to_create_coeficients_into,
-            df_of_primary_coeficient=df_of_primary_coeficient,
-            
-            pilot_index_in_df_to_create_coeficients_into=pilot_index_in_df_to_create_coeficients_into,
-            pilot=pilot
+    df_to_create_coeficients_into.coeficient.fillna(
+        df_to_create_coeficients_into.this_race_coeficient, 
+        inplace=True
+    )
+    
+    df_to_create_coeficients_into["average_coeficient"] = \
+        df_to_create_coeficients_into[
+            ['this_race_coeficient', 'coeficient']
+        ].mean(axis=1)
+    
+    df_to_create_coeficients_into["temp_from_average_coeficient"] =\
+        df_to_create_coeficients_into["average_coeficient"].apply(
+            make_temp_from_average_coeficient,
+            max_temp = max_temp,
+            min_temp = min_temp
         )
-
-        df_to_create_coeficients_into.loc[
-            pilot_index_in_df_to_create_coeficients_into,
-            "average_coeficient"
-        ] = create_avarage_coeficient(
-            this_race_coeficient=df_to_create_coeficients_into.loc[
-                    pilot_index_in_df_to_create_coeficients_into,
-                    "this_race_coeficient"
-                ],
-            pilot_coeficient=df_to_create_coeficients_into.loc[
-                    pilot_index_in_df_to_create_coeficients_into,
-                    "pilot_coeficient"
-                ]
-        ) 
-
-        df_to_create_coeficients_into.loc[
-            pilot_index_in_df_to_create_coeficients_into,
-            "temp_from_average_coeficient"
-        ] = make_temp_from_average_coeficient(
-            average_coeficient=df_to_create_coeficients_into.loc[
-                    pilot_index_in_df_to_create_coeficients_into,
-                    "average_coeficient"
-                ],
-            max_temp=max_temp,
-            min_temp=min_temp
-        )
+    
     return df_to_create_coeficients_into

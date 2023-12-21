ae999bf066e0eb87b49e294eb837e8a1427f4a72
GawarWawar <gawarwawar@gmail.com>
2023-12-21 14:04:26 +0200

    - added docksting to collect_model_records_into_DataFrame in models_transmissions

--- karting_site/analyzer/utils/analyzation_process/models_transmissions.py
+++ karting_site/analyzer/utils/analyzation_process/models_transmissions.py
@@ -16,6 +16,30 @@ def collect_model_records_into_DataFrame(
     columns_names: list = None,
     purge_models_technical_columns:bool = True
 ):
+    """
+    Collect records from a Django model into a DataFrame.
+
+    This function fetches records from a Django model with a specified 'inheritance_id' and
+    creates a DataFrame. Optionally, specific column names can be provided, and technical
+    columns such as 'id' and column that contain 'inheritance_id' can be purged.
+
+    Parameters:
+    - model (models.Model): The Django model from which to collect records.
+    - inheritance_id (int): The value of the column used for inheritance.
+    - column_of_inheritance_id (str): The name of the column used for inheritance.
+    - columns_names (list): Optional list of column names to give the columns in the DataFrame.
+    If not specified, columns in the DataFrame will have the same names as in the model.
+    - purge_models_technical_columns (bool): If True, remove technical columns like 'id' and
+      column that contain 'inheritance_id' from the resulting DataFrame. Default is True.
+
+    Returns:
+    - pd.DataFrame: The DataFrame containing records from the specified Django model.
+
+    Note:
+    The function uses the 'values_list' method to fetch records from the Django model and
+    creates a DataFrame from the records. Optionally, specific column names can be provided,
+    and technical columns can be purged if 'purge_models_technical_columns' is set to True.
+    """
     if columns_names is None:
         meta = model._meta
         columns_names = [
